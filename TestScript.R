b0b6e010e26757441a354c90f634e8f738c8816e
DavidFrenchSG <David.French@gov.scot>
2023-07-27 11:43:03 +0100

    Adding some very basic code

--- TestScript.R
+++ TestScript.R
@@ -1,1 +1,20 @@
-#A blank R script for testing purposes
+#A simple R script for git testing purposes
+#load packages
+library(tidyverse)
+library(ggplot2)
+
+#Define input dataset location
+##Note that CSV files are listed in the gitignore file, to prevent them being uploaded to github.
+Input_dataset_filename <- 'ThreeYears_CN.csv'
+
+#Read in dataset
+Input_dataset <- read.csv(Input_dataset_filename)
+
+#Process data
+Processed_dataset <- Input_dataset %>% 
+  mutate(sampyear=fa_id%%10000)
+
+# Visualise data
+g <- ggplot(data = Processed_dataset) +
+  geom_point(aes(x=fbi, y=wf_co2, colour= as.factor(sampyear)))
+g
