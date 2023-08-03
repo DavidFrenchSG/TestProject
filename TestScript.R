1491b2410c3de918fe5b46fb27843ac2887b8bd6
ColinStevensonSG <Colin.Stevenson@gov.scot>
2023-08-03 12:37:37 +0100

    Initial commit

--- TestScript.R
+++ TestScript.R
@@ -17,4 +17,5 @@ Processed_dataset <- Input_dataset %>%
 # Visualise data
 g <- ggplot(data = Processed_dataset) +
   geom_point(aes(x=fbi, y=wf_co2, colour= as.factor(sampyear)))
-g
+
+## Testing - Hello from Colin!
